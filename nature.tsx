05f5dea7f27cb43d22afd775a5ce7f5ea6ece993
MDulche <dulchemaxence422010@gmail.com>
2025-02-17 11:04:54 +0100

    first commit

---
+++ nature.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.1-99-gec89c545" name="nature" tilewidth="16" tileheight="16" tilecount="720" columns="40">
+ <image source="natureset.png" width="641" height="288"/>
+</tileset>
